307cfebd561a45174c00984a629bcca23b1d44f1
Kathryn Mueller <kathrynmueller@gmail.com>
2018-08-08 10:31:49 -0400

    incresed integer limit for phone

--- db/migrate/20180808140605_change_integer_limit_in_students.rb
+++ db/migrate/20180808140605_change_integer_limit_in_students.rb
@@ -1,5 +1,5 @@
 class ChangeIntegerLimitInStudents < ActiveRecord::Migration[5.2]
   def change
-  	change_column :students, :phone, :integer, limit: 10
+  	change_column :students, :phone, :integer, limit: 8
   end
 end
