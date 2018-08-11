e55ce36482af3a8d159748a2a15acee58dc2a015
Kathryn Mueller <kathrynmueller@gmail.com>
2018-08-11 15:29:09 -0400

    email again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,8 @@ Rails.application.configure do
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'login',
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    :only_path: true,
 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
