cc73a50990c6e8e64e42d8eebb5758c704c078bb
Kathryn Mueller <kathrynmueller@gmail.com>
2018-08-11 16:44:35 -0400

    small improvements before class presentation

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -74,6 +74,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:firstname, :lastname, :status, :phone, :email, :dob, :experience, :interests, :lessontime)
+      params.require(:student).permit(:firstname, :lastname, :status, :phone, :email, :dob, :experience, :interests, :lesson_time)
     end
 end
